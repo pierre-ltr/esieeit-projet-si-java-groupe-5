d6e6975d0c41160d0396cd8efe3cc5d9ffd13cd3
Pierre Leterrier <pierre.leterrier9@gmail.com>
2026-02-10 10:23:42 +0100

    feat: add initial unit test for project sanity check

---
+++ app/src/test/java/com/esieeit/projetsi/AppTest.java
@@ -0,0 +1,12 @@
+package com.esieeit.projetsi;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void sanity_check() {
+        assertTrue(true, "Le projet est bien initialisé");
+    }
+}
